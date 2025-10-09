87f4de9c739e763103e6a8193e2d6b99cc8ae2b3
Andries07 <andriesmoolman07@gmail.com>
2025-10-09 11:39:13 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE='kiosk-fixed-v3';
+const ASSETS=['./','./index.html','./config.json','./background.jpg'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))); self.clients.claim()});
+self.addEventListener('fetch',e=>{
+  const url=new URL(e.request.url);
+  if(url.pathname.endsWith('/config.json')){
+    e.respondWith(fetch(e.request).then(r=>{const c=r.clone(); caches.open(CACHE).then(cc=>cc.put(e.request,c)); return r}).catch(()=>caches.match(e.request)));
+    return;
+  }
+  e.respondWith(caches.match(e.request).then(m=>m||fetch(e.request)));
+});
