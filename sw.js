81df9117768929689b8c5704c8ba6472cf995e62
Andries07 <andriesmoolman07@gmail.com>
2025-10-09 14:56:48 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='kiosk-fixed-v11';
+const CACHE='kiosk-fixed-v1';
 const ASSETS=['./','./index.html','./config.json','./background.jpg'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))); self.clients.claim()});
